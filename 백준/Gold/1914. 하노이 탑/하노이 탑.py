922563ca9da35d769b544f9b6ece6c7812cb726b
seula <urghb12375@gmail.com>
2024-07-09 10:43:20 +0900

    [Gold V] Title: 하노이 탑, Time: 372 ms, Memory: 122380 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.py
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.py
@@ -1,16 +1,17 @@
-num=int(input())
-
-
-def move(n,source,target,auxiliary):
+def hanoi(n,start,via,end):
     if n==1:
-        print(f"{source} {target}")
-    else:   
-            move(n-1,source,auxiliary,target)
-            print(f"{source} {target}")
-            move(n-1,auxiliary,target,source)
+    #원판이 하나일때는 바로 end로 옮김
+        print(f'{start} {end}')
+        return
+    #원판이 2개 이상일때는 n-1개의 원반을 중간기둥으로 옮긴뒤
+    hanoi(n-1,start,end,via)
+    #n 원반을 end로 옮기고
+    print(f'{start} {end}')
+    #중간기둥에 있는 n-1의 원반을 end로 옮긴다
+    hanoi(n-1,via,start,end)
 
-
-
+num=int(input())
+#n=1일때 1번, n=2일때 1+2번, n=3일때 1+2+2^2번... n번일때 1+2+2^2+2^3+...2^n-1 의 일반항
 print(2**num-1)
 if num<=20:
-    move(num,1,3,2)
+    hanoi(num,1,2,3)
