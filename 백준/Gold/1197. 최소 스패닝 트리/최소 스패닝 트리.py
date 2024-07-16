98f0aa876c25ec9c1d2a6b8c9f3719a82edf86ad
seula <urghb12375@gmail.com>
2024-07-16 22:30:31 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 208 ms, Memory: 50000 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,33 +1,26 @@
 import sys
 sys.setrecursionlimit(10**6)
-V,E=map(int,input().split())
-graph=[]
-for _ in range(E):
-    A,B,C=map(int,input().split())
-    graph.append([A,B,C])
-def kruskal(graph):
-    edges= sorted(graph, key=lambda x:x[2])
-    parent=list(range(V+1))
-    mst=[]
-    def find(x):
-        if parent[x]!=x:
-            parent[x]=find(parent[x])
-        return parent[x]
-    def union(a,b):
-        a=find(a)
-        b=find(b)
-        if a<b:
-            parent[b]=a
-        else:
-            parent[a]=b
-    for edge in edges:
-        a,b,weight=edge
-        if find(a)!=find(b):
-            union(a,b)
-            mst.append(edge)
-    return mst
-mst_weight=[]
-mst = kruskal(graph)
-for element in mst:
-    mst_weight.append(element[2])
-print(sum(mst_weight))
+input=sys.stdin.readline
+def find(x):
+    if parent[x]!=x:
+        parent[x]=find(parent[x])
+    return parent[x]
+def union(a,b):
+    a,b=find(a),find(b)
+    if a<b:
+        parent[b]=a
+    else:
+        parent[a]=b
+V,E=map(int,input().strip().split())
+edges= sorted([tuple(map(int,input().strip().split())) for _ in range(E)], key=lambda x:x[2])
+parent=list(range(V+1))
+mst_weight = 0
+edge_count=0
+for a,b,weight in edges:
+    if find(a)!=find(b):
+        union(a,b)
+        mst_weight+=weight
+        edge_count+=1
+        if edge_count==V-1:
+            break
+print(mst_weight)
