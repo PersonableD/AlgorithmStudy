b8468267f81bea310915b93b7f749815829a9a3f
seula <urghb12375@gmail.com>
2024-07-17 20:46:00 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,24 +1,19 @@
-from collections import deque
 import sys
-input=sys.stdin.readline
-N=int(input())
-M=int(input())
-graph=[[] for _ in range(N+1)]
-for _ in range(M):
+sys.setrecursionlimit(10**6)
+n=int(input())
+m=int(input())
+graph=[[] for _ in range(n+1)]
+for _ in range(m):
     a,b = map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
-visited=[False]*(N+1)
-def virus_BFS(start):
-    queue=deque([start])
-    visited[start]=True
-    infected=0
-    while queue:
-        virus=queue.popleft()
-        for i in graph[virus]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i]=True
-                infected+=1
-    return infected
-print(virus_BFS(1))
+visited=[False]*(n+1)
+infected=[]
+def recurvsive_DFS(node,visited,infected):
+    visited[node]=True
+    infected.append(node)
+    for neighbor in graph[node]:
+        if not visited[neighbor]:
+            recurvsive_DFS(neighbor,visited,infected)
+recurvsive_DFS(1,visited,infected)
+print(len(infected) - 1)
