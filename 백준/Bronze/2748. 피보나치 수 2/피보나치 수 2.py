185b2f46acf748c75e0dc9a0dd75b0fddf65ec53
seula <urghb12375@gmail.com>
2024-07-22 12:53:37 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -6,10 +6,8 @@ sums={}
 def f(n):
     if n in sums:
         return sums[n]
-    if n==0:
-        return 0
-    if n==1:
-        return 1
+    if n<=1:
+        return n
     sums[n]= f(n-1)+f(n-2)
     return sums[n]
 print(f(N))
