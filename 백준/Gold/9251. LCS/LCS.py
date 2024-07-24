347777db6b2afbfc6d26f1380391506f14830b22
seula <urghb12375@gmail.com>
2024-07-24 22:54:13 +0900

    [Gold V] Title: LCS, Time: 436 ms, Memory: 55708 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,10 @@
+word1=[0]+list(input().strip())
+word2=[0]+list(input().strip())
+dp=[[0]*(len(word1)) for _ in range(len(word2))]
+for i in range(1,len(word2)):
+    for j in range(1,len(word1)):
+        if word1[j]==word2[i]:
+            dp[i][j]=dp[i-1][j-1]+1
+        else:
+            dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+print(dp[-1][-1])
