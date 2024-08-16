765559de1abbecaa9005e9c13b8fd64146393d6d
seula <urghb12375@gmail.com>
2024-08-16 13:48:40 +0900

    [Gold IV] Title: 오큰수, Time: 972 ms, Memory: 155536 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -6,17 +6,9 @@ nums = list(map(int, input().split()))
 result = [-1] * N
 stack = []
 
-# 배열을 거꾸로 순회합니다
-for i in range(N - 1, -1, -1):
-    # 현재 숫자보다 작은 수는 스택에서 제거합니다
-    while stack and stack[-1] <= nums[i]:
-        stack.pop()
-    
-    # 스택이 비어있지 않다면, 스택의 top이 오큰수입니다
-    if stack:
-        result[i] = stack[-1]
-    
-    # 현재 숫자를 스택에 추가합니다
-    stack.append(nums[i])
+for i in range(N):
+    while stack and nums[stack[-1]] < nums[i]:
+        result[stack.pop()] = nums[i]
+    stack.append(i)
 
 print(*result)
