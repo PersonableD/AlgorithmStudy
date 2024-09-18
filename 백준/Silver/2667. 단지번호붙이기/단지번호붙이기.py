d345232de7a482383a9c25a89619233f35727b66
seula <urghb12375@gmail.com>
2024-09-18 17:10:21 +0900

    [Silver I] Title: 단지번호붙이기, Time: 52 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -6,21 +6,20 @@ apt =[list(input().strip())for _ in range(N)]
 dx=[0,0,1,-1]
 dy=[1,-1,0,0]
 queue=deque()
-visited=[[False]*N for _ in range(N+1)]
 house=[]
 for i in range(N):
     for j in range(N):
         cnt=0
-        if visited[i][j]==False and apt[i][j]=="1":
+        if apt[i][j]!="-1" and apt[i][j]=="1":
             queue.append((i,j))
             while queue:
                 x,y=queue.popleft()
-                visited[x][y]=True
+                apt[x][y]="-1"
                 for k in range(4):
                     nx,ny= dx[k]+x,dy[k]+y
-                    if 0<=nx<N and 0<= ny<N and apt[nx][ny]=="1" and visited[nx][ny]==False:
+                    if 0<=nx<N and 0<= ny<N and apt[nx][ny]=="1" and apt[nx][ny]!="-1":
                         queue.append((nx,ny))
-                        visited[nx][ny]=True
+                        apt[nx][ny]="-1"
                 cnt+=1
             house.append(cnt)
 house.sort()
