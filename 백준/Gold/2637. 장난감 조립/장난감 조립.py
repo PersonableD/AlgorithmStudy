6714bd76e6bb9ee01426079018716780031a22ca
seula <urghb12375@gmail.com>
2024-09-23 15:22:37 +0900

    [Gold II] Title: 장난감 조립, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2637. 장난감 조립/장난감 조립.py
+++ 백준/Gold/2637. 장난감 조립/장난감 조립.py
@@ -1,38 +1,30 @@
 import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-
-N = int(input())
-M = int(input())
-parts = {}
-parts_m = set()
-
+N=int(input())
+M=int(input())
+parts={}
+parts_m=set()
+answer={}
 for _ in range(M):
-    X, Y, K = map(int, input().split())
+    X,Y,K = map(int,input().split())
     parts_m.add(X)
     if X not in parts:
-        parts[X] = {}
-    parts[X][Y] = K
-
+        parts[X]={}
+    parts[X][Y]=K
 memo = {}
-
 def assemble(num):
     if num in memo:
         return memo[num]
-    
     if num not in parts_m:
-        return {num: 1}
-    
+       return {num:1}
     result = {}
-    for part, count in parts[num].items():
+    for part,count in parts[num].items():
         sub_result = assemble(part)
         for sub_part, sub_count in sub_result.items():
-            result[sub_part] = result.get(sub_part, 0) + sub_count * count
-    
-    memo[num] = result
+            result[sub_part] = result.get(sub_part,0) + sub_count *count
+    memo[num] =result
     return result
-
 answer = assemble(N)
-
-for key in sorted(answer.keys()):
-    print(f"{key} {answer[key]}")
+for key,value in sorted(answer.items()):
+    print(f"{key} {value}")
