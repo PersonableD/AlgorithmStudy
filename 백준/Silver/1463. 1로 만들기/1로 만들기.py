ca62501510855223374405035ac124aa5eb6b8e1
seula <urghb12375@gmail.com>
2024-09-30 12:27:05 +0900

    [Silver III] Title: 1로 만들기, Time: 444 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,16 +1,16 @@
 import sys
-N=int(input())
-input= sys.stdin.readline
-dp=[float("inf")]*(N+1)
-for i in range(1,N+1):
-    if i == 1 :
-        dp[i]=0
-    elif i%2 ==0 and i%3 ==0:
-        dp[i]=min(1+dp[i//2],1+dp[i//3])
-    elif i%2 ==0:
-        dp[i]=min(1+dp[i//2],dp[i-1]+1)
-    elif i%3 ==0:
-        dp[i]=min(1+dp[i//3],dp[i-1]+1)
+
+N = int(sys.stdin.readline())
+dp = [0] * (N + 1)
+
+for i in range(2, N + 1):
+    if i % 2 == 0 and i % 3 == 0:
+        dp[i] = min(dp[i // 2], dp[i // 3]) + 1
+    elif i % 2 == 0:
+        dp[i] = min(dp[i // 2], dp[i - 1]) + 1
+    elif i % 3 == 0:
+        dp[i] = min(dp[i // 3], dp[i - 1]) + 1
     else:
-        dp[i] = dp[i-1]+1
+        dp[i] = dp[i - 1] + 1
+
 print(dp[N])
