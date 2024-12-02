a12bf17de992e0ff6acb55dde4dcb3499d501684
seula <urghb12375@gmail.com>
2024-12-02 18:55:51 +0900

    [Silver II] Title: 차이를 최대로, Time: 136 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -1,13 +1,26 @@
-import itertools
+import sys
+input= sys.stdin.readline
 N=int(input())
 arr=list(map(int,input().split()))
 i=0
+visited = [False]*len(arr)
+pickednum=[]
 result=0
-for perm in itertools.permutations(arr):
-    temp=0
-    perm_list = list(perm)
-    for i in range(len(perm_list)-1):
-        temp += abs(perm_list[i]-perm_list[i+1])
-    temp = max(temp,result)
-    result=temp
+def permutation(cnt,depth):
+    global result
+    if cnt ==depth:
+        temp=0
+        for i in range(len(pickednum)-1):
+            temp += abs(pickednum[i]-pickednum[i+1])
+        temp = max(temp,result)
+        result =temp
+        return
+    for index in range(len(arr)):
+        if visited[index]==False:
+            visited[index]=True
+            pickednum.append(arr[index])
+            permutation(cnt+1,depth)
+            pickednum.pop()
+            visited[index]=False
+permutation(0,len(arr))
 print(result)
