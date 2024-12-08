d91f18ed428600438e3e6270dd861c12ce3da0df
seula <urghb12375@gmail.com>
2024-12-08 20:44:11 +0900

    [Silver I] Title: 포도주 시식, Time: 288 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,15 +1,12 @@
 n=int(input())
-wine =[int(input()) for _ in range(n)]
-if n==1:
-    print(wine[0])
-    exit()
-elif n==2:
-    print(wine[0]+wine[1])
-    exit()
+wine=[int(input()) for _ in range(n)]
 dp=[0]*n
 dp[0]=wine[0]
-dp[1]=wine[0]+wine[1]
-dp[2]=max(dp[1],wine[0]+wine[2],wine[1]+wine[2])
-for i in range(3,n):
-    dp[i] = max(dp[i-1],dp[i-2]+wine[i],dp[i-3]+wine[i-1]+wine[i])
+if n>1:
+    dp[1]=wine[0]+wine[1]
+if n>2:
+    dp[2]=max(wine[0]+wine[1],wine[1]+wine[2],wine[0]+wine[2])
+if n>3:
+    for i in range(3,n):
+        dp[i]=max(dp[i-1],dp[i-2]+wine[i],dp[i-3]+wine[i-1]+wine[i])
 print(dp[n-1])
