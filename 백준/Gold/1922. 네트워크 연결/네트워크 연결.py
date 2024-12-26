b189dd041bfc5d5510e8f0bbf16eac3ccdff01f7
seula <urghb12375@gmail.com>
2024-12-26 11:46:58 +0900

    [Gold IV] Title: 네트워크 연결, Time: 176 ms, Memory: 49880 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -15,13 +15,13 @@ V=int(input())
 E=int(input())
 edges= sorted([tuple(map(int,input().strip().split())) for _ in range(E)], key=lambda x:x[2])
 parent=list(range(V+1))
-mst_weight = []
+mst_weight = 0
 edge_count=0
 for a,b,weight in edges:
     if find(a)!=find(b):
         union(a,b)
-        mst_weight.append(weight)
+        mst_weight+=weight
         edge_count+=1
         if edge_count==V-1:
             break
-print(sum(mst_weight))
+print(mst_weight)
