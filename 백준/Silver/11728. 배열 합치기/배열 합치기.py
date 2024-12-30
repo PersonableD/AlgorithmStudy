e7fad9074b89d93a687575bb34be833eb1e6210d
seula <urghb12375@gmail.com>
2024-12-30 11:37:52 +0900

    [Silver V] Title: 배열 합치기, Time: 1284 ms, Memory: 187984 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.py
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -1,6 +1,7 @@
+import sys
+input=sys.stdin.readline
 N,M=map(int,input().split())
 arr1=list(map(int,input().split()))
 arr2=list(map(int,input().split()))
-result=arr1+arr2
-result.sort()
+result=sorted(arr1+arr2)
 print(*result)
